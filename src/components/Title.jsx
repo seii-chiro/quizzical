0dbdcb5f3a52a25e7c64cfb9c18d79e74f4d2a82
John Rhay <javier.johnrhay@gmail.com>
2024-08-10 22:16:51 +0800

    v2

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ const Title = ({startQuiz}) => {
   return (
     <div className="title-container">
         <h1 className="title">Quizzical</h1>
-        <p className="description">I'll think of a description later</p>
+        <p className="description">Test your general information knowledge!</p>
         <button onClick={startQuiz} className="start-quiz-btn">Start Quiz</button>
     </div>
   )
